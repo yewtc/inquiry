95d53b90f150210fed0ad24cdd70f7cde88e5744
E. Choroba <choroba@still.ms.mff.cuni.cz>
2012-09-06 13:28:27 +0200

    validation - all questions must have an answer

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -5,7 +5,22 @@ function checking (q, i, u) {
     div = $('div[name="fold' + q + '"]');
     if (u && $(div).attr('hidden')) {
         $(div).removeAttr('hidden');
+        $(div).find('input').first().attr('checked', 'checked');
     } else {
         $(div).attr('hidden', 'hidden');
+        $(div).find('input').removeAttr('checked');
     }
+}
+
+function validate () {
+    var ps = $('div[name^="p"]');
+    for (i=0; i< ps.length ; i++ ) {
+        var p = ps[i];
+        var checked = $(p).find('input:checked');
+        if (checked.length == 0) {
+            alert('Chybi odpoved ' + (i + 1));
+            return false;
+        }
+    }
+    return true;
 }
