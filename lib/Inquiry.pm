e363c49b213a4aa3587ea62a2b7d0f63a4ae83b4
Jan Stepanek <jan.stepanek@matfyz.cz>
2012-09-11 12:59:13 +0200

    towards by_one: results not remembered yet

--- lib/Inquiry.pm
+++ lib/Inquiry.pm
@@ -2,6 +2,7 @@ package Inquiry;
 
 use strict;
 use warnings;
+use utf8;
 
 use Survey;
 use Results;
@@ -16,14 +17,39 @@ use constant {
     DB_FILE => 'inquiry.db',
 };
 
-get '/' => sub {
+sub init {
     my $survey = Survey->new('anketa.txt');
 
     # Now we know the number of questions:
     Results->new(DB_FILE)->init(scalar keys %{$survey});
+    return $survey;
+}
 
-    template 'index', { questions => $survey->shake(QUESTION_COUNT) };
+
+any [qw/get post/] => '/' => sub {
+    unless (session('shaken')) {
+        session shaken => init()->shake(QUESTION_COUNT);
+    }
+    if (session('current')) {
+#        die Dumper {params()};
+        session current => 1 + session('current') if 'Další' eq (param('next') // q());
+    } else {
+        session current => 1;
+    }
+    template 'by_one', {current => session('current'),
+                        shaken  => session('shaken'),
+                        max     => QUESTION_COUNT};
 };
+
+
+post '/submit_one' => sub {
+};
+
+
+get '/all' => sub {
+    template 'index', { questions => init()->shake(QUESTION_COUNT) };
+};
+
 
 get '/submit' => sub {
     my $results = Results->new(DB_FILE, request->address);
