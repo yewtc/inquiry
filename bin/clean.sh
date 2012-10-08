99de0a128f5556e70bc6df46363dc64b020a6e57
E. Choroba <choroba@still.ms.mff.cuni.cz>
2012-10-08 02:04:20 +0200

    cleanup script

---
+++ bin/clean.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -e
+bindir=$(readlink -f "${0%/*}")
+rm -rf "$bindir"/../{inquiry,logs,sessions}.old
+[[ -f "$bindir"/../inquiry.db ]] && \
+    mv "$bindir"/../inquiry.db "$bindir"/../inquiry.old
+[[ -d "$bindir"/../logs ]] && \
+    mv "$bindir"/../logs "$bindir"/../logs.old
+[[ -d  "$bindir"/../sessions ]] && \
+    mv "$bindir"/../sessions "$bindir"/../sessions.old
+
+
+exit 0
