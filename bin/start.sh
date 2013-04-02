400647844e165fa0b3cebd7c27701f0b9d491cec
Jan Stepanek <jan.stepanek@matfyz.cz>
2013-04-02 18:55:33 +0200

    more documentation

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+PORT=8867
 bindir=$(readlink -f "${0%/*}")
 [[ -d "$bindir"/../logs ]] || mkdir "$bindir"/../logs
-plackup -s Starman -w 4 -a "$bindir"/app.pl -p 8867 -E production 2> "$bindir"/../logs/start.log
+plackup -s Starman -w 4 -a "$bindir"/app.pl -p $PORT -E production 2> "$bindir"/../logs/start.log
