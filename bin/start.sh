0e4ee17c90cd56db7678184cdb9eb77b54aa4e45
Jan Stepanek <choroba@clay.ms.mff.cuni.cz>
2013-05-29 13:24:45 +0200

    start.sh can be called from anywhere

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 PORT=8867
 bindir=$(readlink -f "${0%/*}")
-[[ -d "$bindir"/../logs ]] || mkdir "$bindir"/../logs
-plackup -s Starman -w 4 -a "$bindir"/app.pl -p $PORT -E production 2> "$bindir"/../logs/start.log
+
+cd "$bindir"/.. || exit 1
+[[ -d logs ]] || mkdir logs
+plackup -s Starman -w 4 -a bin/app.pl -p $PORT -E production 2> logs/start.log
